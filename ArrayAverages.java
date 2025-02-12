8fe822983de736e27ae977529b1757677368420b
jMassie1 <jaden.massie@stu.daviess.kyschools.us>
2025-02-12 03:44:36 +0000

    Finished

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,11 +9,21 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    int avg = 0;
+    for(int[] row: ray){
+      avg = 0;
+      for(int col: row){
+        avg+=col;
+      }
+      avg = avg/row.length;
+      System.out.println("The average of " + row + "is: " + avg);
+    }
   }
 
   public void columnAverages(){
-
+    //Column major order
+    //Take each column and go through all of the rows
+    //Top down left to right
   }
 
   public void arrayAverage(){
@@ -25,5 +35,10 @@ public class ArrayAverages{
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+    //goes from row on the outside to column on the inside
+    //column in the outside to row on the outside
+    
+
+    return "";
   }
 }
